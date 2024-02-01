90331fb2b8d7d1bf846db8d55c59165b69b322a4
ROBERTO <robertojt6@gmail.com>
2024-02-01 21:17:05 +0100

    firebase test

---
+++ 2EVA/FireBase/app/src/main/java/com/rsoftware/firebase/MainActivity.java
@@ -0,0 +1,46 @@
+package com.rsoftware.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+
+
+
+
+
+    }
+
+    public void register(View view) {
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                Toast.makeText(this, "Sucesso", Toast.LENGTH_SHORT).show();
+            }else{
+
+                Toast.makeText(this, "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
